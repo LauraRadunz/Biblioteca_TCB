660c3982622adae9cc6abd5d23c6fbcd1fabf178
LauraRadunz <lauraradunzpedro@gmail.com>
2025-11-12 21:29:08 -0300

    Conexão com o banco de dados da nuvem

--- src/main/java/br/edu/ifpr/biblioteca/model/dao/ConnectionFactory.java
+++ src/main/java/br/edu/ifpr/biblioteca/model/dao/ConnectionFactory.java
@@ -1,4 +1,4 @@
-package br.edu.ifpr.biblioteca.model.dao;
+package br.edu.ifpr.biblioteca.model.dao; // O seu pacote
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,18 +6,37 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/Biblioteca";
-    private static final String USER = "root";
-    private static final String PASSWORD = "LRPBDlrpbd2828@";
+    // --- C do TiDB Cloud ---
+    
+    // 1. Host (Endereço)
+    private static final String HOST = "gateway01.us-east-1.prod.aws.tidb.cloud";
+    
+    // 2. Port (Porta)
+    private static final String PORT = "4000";
+    
+    // 3. Database (Banco).
+    private static final String DATABASE = "test";
+    
+    // 4. User (Usuário)
+    private static final String USER = "2QjTfCmUjXizSPv4.root";
+    
+    // 5. PASSWORD (Senha)
+    private static final String PASSWORD = "uf783Usq9z1PGvoa"; 
+    
+    // --- Fim da Configuração ---
+
+    // URL de conexão
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?useSSL=true&requireSSL=true";
 
     public static Connection getConnection() {
         try {
-            System.out.println("Criando uma nova conexão com o banco...");
-            
+            // A cada chamada, ele cria uma nova conexão segura com a nuvem
+            System.out.println("Conectando ao banco de dados na nuvem...");
             return DriverManager.getConnection(URL, USER, PASSWORD);
-
+            
         } catch (SQLException e) {
-            throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+            // Lança uma exceção para quem chamou saber que deu erro
+            throw new RuntimeException("Erro ao conectar ao banco de dados na nuvem", e);
         }
     }
 }
